89fb6eb05ab46dda2646f79723f18d5f29debbeb
Manish Rathi <manishrathi19902013@gmail.com>
2019-11-15 15:28:11 +0100

    Detach currency-formatter from Amount.

--- Source/CurrencyDemo/CurrencyDemo/Reusable/Amount.swift
+++ Source/CurrencyDemo/CurrencyDemo/Reusable/Amount.swift
@@ -4,17 +4,8 @@ public struct Amount {
     let price: String
     let currencyType: CurrencyType
 
-    private static var currencyFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = 2
-
-        return formatter
-    }()
-
     private var formatter: NumberFormatter {
-        let formatter = Amount.currencyFormatter
+        let formatter = NumberFormatter.currencyFormatter
         formatter.locale = Locale(countryCode: currencyType.code)
 
         return formatter
---
+++ Source/CurrencyDemo/CurrencyDemo/Reusable/NumberFormatterExtensions.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension NumberFormatter {
+    static var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+
+        return formatter
+    }()
+}
